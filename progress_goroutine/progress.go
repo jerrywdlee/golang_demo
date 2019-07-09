6e913a349a9c715ab3a0a4ed386b6f9bc6d4c3bf
Jerry Lee <lee@idoctor.co.jp>
2019-07-09 20:12:45 +0900

    change name

--- progress_goroutine/progress.go
+++ progress_goroutine/progress.go
@@ -7,11 +7,6 @@ import (
 )
 
 func main() {
-	// for i := 0; i < 100; i++ {
-	// 	fmt.Printf("\rOn %d/10", i)
-	// 	fmt.Printf("\r\t\tOn %d/10", i)
-	// 	time.Sleep(10 * time.Millisecond)
-	// }
 	quits := make([]chan bool, 5)
 	for j := 0; j < 5; j++ {
 		quit := make(chan bool)
@@ -29,7 +24,6 @@ func ShowProgressBar(index int, quit chan bool) {
 	for i := 0; i < 100; i++ {
 		tempStr := "\r" + strings.Repeat("\t\t", index) + "Chan%d: %d/100"
 		fmt.Printf(tempStr, index, i)
-		// fmt.Printf("\r\t\tOn %d/10", i)
 		time.Sleep(50 * time.Millisecond)
 	}
 	quit <- true
